c76f62b8241235a31cf8da0a5627796074cfeb44
ShinJeongBeom <dingfan36@gmail.com>
2025-02-26 02:17:17 +0900

    Feat: FestivalPost Repository 생성

---
+++ src/main/java/com/example/onculture/domain/event/repository/FestivalPostRepository.java
@@ -0,0 +1,11 @@
+package com.example.onculture.domain.event.repository;
+
+import com.example.onculture.domain.event.model.FestivalPost;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface FestivalPostRepository extends JpaRepository<FestivalPost, Long> {
+    // 위치(keyword)를 포함하는 게시글을 검색 (필드명 festivalLocation 사용)
+    List<FestivalPost> findByFestivalLocationContaining(String keyword);
+}
