8d9c602c6b46a7cbb129645b5a84c8cecfbebe2f
MadPumpkin3 <fpdjxpa37@gmail.com>
2025-03-04 23:39:40 +0900

    Feat: OAuth2 관련 환경 변수 설정, 프론트 중심의 OAuth2 구현 방식 세팅(대비용)

---
+++ src/main/java/com/example/onculture/domain/oauth/dto/OAuthLoginRequest.java
@@ -0,0 +1,18 @@
+package com.example.onculture.domain.oauth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+// 프론트에서 인증 코드 받을 때 사용하는 클래스
+/*
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OAuthLoginRequest {
+    private String provider; // "google" 또는 "kakao"
+    private String authorizationCode;
+}
+ */
