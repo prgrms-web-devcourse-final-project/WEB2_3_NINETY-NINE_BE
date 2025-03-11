40907e70f9972025e80f09905e1ac55e09eb112f
TaeyoungKim <teayoung789@naver.com>
2025-03-11 15:40:07 +0900

    cors path 허용

--- src/main/java/com/example/onculture/global/config/CorsConfig.java
+++ src/main/java/com/example/onculture/global/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
 			.allowedOrigins("http://localhost:3000", "http://localhost:5173", "https://on-culture.netlify.app") // 로컬 개발 환경 허용
-			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
 			.allowedHeaders("*") // 모든 요청 헤더 허용
 			.allowCredentials(true);
 	}
